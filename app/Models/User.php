f3c5800423082f510b258d0bdb59ba6bed75e49b
Аня <Sytuagina@sputnikfund.ru>
2021-11-01 17:35:22 +0700

    [user_master_refresh_token] add actionLoginToService to User

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,9 @@ use App\Exceptions\EmptyPasswordException;
 use App\Exceptions\PasswordHashException;
 use Egal\Auth\Tokens\UserMasterRefreshToken;
 use Egal\Auth\Tokens\UserMasterToken;
+use Egal\Auth\Tokens\UserServiceToken;
 use Egal\AuthServiceDependencies\Exceptions\LoginException;
+use Egal\AuthServiceDependencies\Exceptions\UserNotIdentifiedException;
 use Egal\AuthServiceDependencies\Models\User as BaseUser;
 use Egal\Model\Traits\UsesUuidKey;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -107,6 +109,31 @@ class User extends BaseUser
             'user_master_token' => $umt->generateJWT(),
             'user_master_refresh_token' => $umrt->generateJWT()
         ];
+    }
+
+    public static function actionLoginToService(string $token, string $serviceName): string
+    {
+        /** @var \Egal\Auth\Tokens\UserMasterToken $umt */
+        $umt = UserMasterToken::fromJWT($token, config('app.service_key'));
+        $umt->isAliveOrFail();
+
+        /** @var \Egal\AuthServiceDependencies\Models\User $user */
+        $user = static::query()->find($umt->getAuthIdentification());
+        $service = Service::find($serviceName);
+
+        if (!$user) {
+            throw new UserNotIdentifiedException();
+        }
+
+        if (!$service) {
+            throw new LoginException('Service not found!');
+        }
+
+        $ust = new UserServiceToken();
+        $ust->setSigningKey($service->getKey());
+        $ust->setAuthInformation($user->generateAuthInformation());
+
+        return $ust->generateJWT();
     }
 
     public function roles(): BelongsToMany
